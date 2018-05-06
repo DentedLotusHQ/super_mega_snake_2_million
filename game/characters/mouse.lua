fbbee8fe054d3e3f1659266b862785ca3120eae4
craig <cisenor1@gmail.com>
2018-05-05 23:14:44 -0700

    Remove print statement

---
+++ game/characters/mouse.lua
@@ -0,0 +1,23 @@
+local Class = require "lib.middleclass"
+local Mouse = Class("Mouse")
+local Vector2 = require "utility.Vector2"
+local graphics = love.graphics
+
+function Mouse:get_tile_position()
+  math.randomseed(os.time())
+  local x = math.random(0, graphics.getWidth())
+  local y = math.random(80, graphics.getHeight())
+  local newX = math.floor(x / 16) * 16
+  local newY = math.floor(y / 16) * 16
+  return Vector2:new(newX, newY)
+end
+function Mouse:initialize()
+  self.position = self:get_tile_position()
+  self.image = graphics.newImage("assets/mouse.png")
+end
+
+function Mouse:draw()
+  graphics.draw(self.image, self.position.x, self.position.y)
+end
+
+return Mouse
