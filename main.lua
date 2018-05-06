35708bdcabad0a477801bfdfbdad2a0519360643
craig <cisenor1@gmail.com>
2018-05-05 23:33:18 -0700

    Highscore and Pause

--- main.lua
+++ main.lua
@@ -2,7 +2,6 @@ local input = require("input/input"):new("keyboard")
 local map = require("map/map"):new("assets/basemap.lua")
 local timer = require("game.Timer"):new()
 local gameManager = require("game.GameManager"):new(timer)
-local mouse = require("game.characters.mouse"):new()
 local map = require("map/map"):new("assets/basemap.lua")
 function love.load()
 end
@@ -10,12 +9,11 @@ end
 function love.update(dt)
   local xy = input:getMovement()
   gameManager:update(dt)
+  map:update(dt)
 end
 
 function love.draw()
-  ui:draw()
-  mouse:draw()
-  map:update(dt)
   map:draw()
+  ui:draw()
   mouse:draw()
 end
