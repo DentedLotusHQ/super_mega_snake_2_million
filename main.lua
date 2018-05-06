e5cab68017aafc42af87ca1e1a6a3b16299bddf9
craig <cisenor1@gmail.com>
2018-05-05 20:50:22 -0700

    Add scoring header.

--- main.lua
+++ main.lua
@@ -1,12 +1,12 @@
-local screen = require("models/ui/Screen"):new()
 local input = require("input/input"):new("keyboard")
-
+local ui = require("models.ui.UI"):new()
 function love.load()
 end
 
 function love.update(dt)
-    local xy = input:getMovement()
+  local xy = input:getMovement()
 end
 
 function love.draw()
+  ui:draw()
 end
