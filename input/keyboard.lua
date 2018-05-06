55fa3ab7128f02075e8abfe8ad881730fc77066a
craig <cisenor1@gmail.com>
2018-05-06 10:06:41 -0700

    Can I fix it?

--- input/keyboard.lua
+++ input/keyboard.lua
@@ -3,44 +3,20 @@ local class = require "middleclass"
 local Keyboard = class("Keyboard")
 
 function Keyboard:initialize()
-<<<<<<< 569b3d30a4c3cca83f6668b74a3089928fd32326
 end
 
 function Keyboard:getMovement()
-    if love.keyboard.isDown("w") then
-        self.player.y = self.player.y - 1
-    end
-    if love.keyboard.isDown("a") then
-        self.player.x = self.player.x - 1
-    end
-    if love.keyboard.isDown("s") then
-        self.player.y = self.player.y + 1
-    end
-    if love.keyboard.isDown("d") then
-        self.player.x = self.player.x + 1
-    end
-=======
-  self.xyTable = {}
-end
-
-function Keyboard:getMovement()
-  self.xyTable["x"] = 0
-  self.xyTable["y"] = 0
-
   if love.keyboard.isDown("w") then
-    self.xyTable["y"] = self.xyTable["y"] - 1
+    self.player.y = self.player.y - 1
   end
   if love.keyboard.isDown("a") then
-    self.xyTable["x"] = self.xyTable["x"] - 1
+    self.player.x = self.player.x - 1
   end
   if love.keyboard.isDown("s") then
-    self.xyTable["y"] = self.xyTable["y"] + 1
+    self.player.y = self.player.y + 1
   end
   if love.keyboard.isDown("d") then
-    self.xyTable["x"] = self.xyTable["x"] + 1
+    self.player.x = self.player.x + 1
   end
-
-  return self.xyTable
->>>>>>> Highscore and Pause
 end
 return Keyboard
--- main.lua
+++ main.lua
@@ -1,8 +1,8 @@
 local input = require("input/input"):new("keyboard")
-local ui = require("models.ui.UI"):new()
+local map = require("map/map"):new("assets/basemap.lua")
 local timer = require("game.Timer"):new()
 local gameManager = require("game.GameManager"):new(timer)
-local map = require("map/map"):new("assets/basemap.lua")
+local ui = require("models.ui.UI"):new(gameManager)
 local mouse = require("game.characters.mouse"):new()
 function love.load()
 end
