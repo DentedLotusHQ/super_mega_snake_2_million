3b5636f4056c91ea1266298c52033c3612c34a96
craig <cisenor1@gmail.com>
2018-05-05 23:14:44 -0700

    Remove print statement

--- map/map.lua
+++ map/map.lua
@@ -3,7 +3,6 @@ local sti = require "lib/sti"
 local Map = class("Map")
 
 function Map:initialize(map_file)
-<<<<<<< HEAD
   self.map = sti(map_file)
 
   self.snake = require("map/snake"):new(self.map)
@@ -20,24 +19,6 @@ end
 function Map:update(dt)
   self.map:update(dt)
   self.snake:update()
-=======
-    self.map = sti(map_file)
-
-    self.snake = require("map/snake"):new(self.map)
-
-    -- Remove player so it doesn't stick on map
-    self.map:removeLayer("Player")
-end
-
-function Map:draw()
-    self.map:draw()
-    self.snake:draw()
-end
-
-function Map:update(dt)
-    self.map:update(dt)
-    self.snake:update()
->>>>>>> 473bc5c... Add basemap and snek
 end
 
 return Map
