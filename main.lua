c1c2ab097dc4cabc4ec57f3496c1b6c2b34d275d
craig <cisenor1@gmail.com>
2018-05-05 23:14:44 -0700

    Mouse generates at random.

--- main.lua
+++ main.lua
@@ -1,14 +1,19 @@
 local screen = require("models/ui/Screen"):new()
 local input = require("input/input"):new("keyboard")
 local map = require("map/map"):new("assets/basemap.lua")
-
+local ui = require("models.ui.UI"):new()
+local timer = require("game.Timer"):new()
+local gameManager = require("game.GameManager"):new(timer)
+local mouse = require("game.characters.mouse"):new()
 function love.load()
 end
 
 function love.update(dt)
-    map:update(dt)
+  map:update(dt)
 end
 
 function love.draw()
-    map:draw()
+  map:draw()
+  ui:draw()
+  mouse:draw()
 end
