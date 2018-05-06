f49e02692793097ea00ac931a9a832692fb58f49
craig <cisenor1@gmail.com>
2018-05-05 23:24:20 -0700

    Add basemap and snek

--- main.lua
+++ main.lua
@@ -3,6 +3,8 @@ local ui = require("models.ui.UI"):new()
 local timer = require("game.Timer"):new()
 local gameManager = require("game.GameManager"):new(timer)
 local mouse = require("game.characters.mouse"):new()
+local map = require("map/map"):new("assets/basemap.lua")
+
 function love.load()
 end
 
@@ -14,4 +16,6 @@ end
 function love.draw()
   ui:draw()
   mouse:draw()
+  map:update(dt)
+  map:draw()
 end
